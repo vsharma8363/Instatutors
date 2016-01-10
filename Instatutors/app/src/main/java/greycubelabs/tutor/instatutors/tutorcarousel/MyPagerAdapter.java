1746db496504e3ec6e3639195e464fa0a7916719
Vikram Sharma <vsharma8363@gmail.com>
2016-01-09 18:04:43 -0800

    SIDEBARUPDATE

--- Instatutors/app/src/main/java/greycubelabs/tutor/instatutors/tutorcarousel/MyPagerAdapter.java
+++ Instatutors/app/src/main/java/greycubelabs/tutor/instatutors/tutorcarousel/MyPagerAdapter.java
@@ -5,7 +5,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 
-import greycubelabs.tutor.instatutors.R;
 
 
 public class MyPagerAdapter extends FragmentPagerAdapter implements
