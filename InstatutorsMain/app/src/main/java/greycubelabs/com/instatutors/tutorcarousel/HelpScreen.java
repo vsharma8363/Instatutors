081f29fd25f54d669dc426631be451f8d9e83531
Instatutors <nishant@Nishants-MacBook-Air.local>
2016-01-20 12:09:54 -0800

    Help

---
+++ InstatutorsMain/app/src/main/java/greycubelabs/com/instatutors/tutorcarousel/HelpScreen.java
@@ -0,0 +1,23 @@
+package greycubelabs.com.instatutors.tutorcarousel;
+
+/**
+ * Created by nishant on 1/20/16.
+ */
+public class HelpScreen
+{
+/*
+1) FAQ
+    - I don't see the subject I need help in
+    - I can't find a tutor for my subject
+    - I forgot my password
+    - I forgot my username
+    - How do I log out of my account
+    - How to change my username
+    - How to change my password
+    -
+2) Report a problem
+3) Suggest an update
+4)
+
+ */
+}
